a27859482accb5d24f61a8e80a9ada98817b7daa
GlitchwareArron <arron.walker@zbfg.co.uk>
2025-12-09 10:16:59 +0000

    Created function to add entry

---
+++ config.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func check_config() -> void:
+	pass
+	
+func load_config() -> void:
+	pass
+
+func reset_config() -> void:
+	pass
+	
--- main_screen.gd
+++ main_screen.gd
@@ -1,26 +1,24 @@
 extends Control
 
-@onready var test_button:Button = $TestButton
+@onready var entry_template:PackedScene = preload("res://entry.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	test_button.pressed.connect(test_launch)
+	load_entry("Konsole","konsole")
 	pass # Replace with function body.
 
 func launch(target:String,arguments:Array = []) -> void:
-	OS.create_process(target,arguments,false)
+	var _pid = OS.create_process(target,arguments,false)
+	print(_pid)
 	return
 	
 func close() -> void:
 	return
 	
-func load_entry() -> void:
-	return
-
-func test_functions() -> void:
-	
-	return
-
-func test_launch() -> void:
-	launch("konsole")
+func load_entry(target_name:String,target_path:String) -> void:
+	var _entry:Entry = entry_template.instantiate()
+	_entry.text = target_name
+	_entry.target = target_path
+	_entry.launch.connect(launch)
+	add_child(_entry)
 	return
