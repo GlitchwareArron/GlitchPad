aac46400d1da2a8e5a27ff99bf630badceefdd74
GlitchwareArron <arron.walker@zbfg.co.uk>
2025-12-09 11:03:20 +0000

    Default config handling

--- config.gd
+++ config.gd
@@ -3,11 +3,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_config()
+	print(check_config())
 	pass # Replace with function body.
 
-func check_config() -> void:
-	pass
+func check_config() -> String:
+	if !(FileAccess.file_exists("user://config.yaml")):
+		var _file = DirAccess.copy_absolute("res://default_config/config.yaml","user://config.yaml")
+		return "Default config set"
+	return "Config Exists"
 	
 func load_config() -> void:
 	var _import = YAML.load_file("res://default_config/config.yaml")
